d7a9555fae7a344eb262ad820b0631ab1f339a39
diegoadrianm <diegomorduar@gmail.com>
2024-11-29 14:53:23 -0600

    Dynamic URLs and views

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("january", views.january, name="january"),
-    path("february", views.february, name="february"),
-    path("march", views.march, name="march"),
+    path("", views.menu, name="menu_challenges"),
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
+month_challenges = {
+    "january": "Eat no sugar for the entire month!",
+    "february": "Walk for at least 30 minutes every day!",
+    "march": "Read a book for at least 40 minutes!",
+    "april": "Eat healthy",
+    "may": "Eat no sugar for the entire month!",
+    "june": "Walk for at least 30 minutes every day!",
+    "july": "Read a book for at least 40 minutes!",
+    "august": "Eat healthy",
+    "september": "Eat no sugar for the entire month!",
+    "october": "Walk for at least 30 minutes every day!",
+    "november": "Read a book for at least 40 minutes!",
+    "dicember": "Eat healthy"
+}
 
-def january(request):
-    return HttpResponse("Eat no sugar for the entire month!")
+
+def menu(request):
+    months = list(month_challenges.keys())
+    list_items = ""
+    for month in months:
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{
+            month.capitalize()}</a></li>"
+    return HttpResponse(f"<ul>{list_items}</ul>")
 
 
-def february(request):
-    return HttpResponse("Walk for at least 30 minutes every day!")
+def monthly_challenge_by_number(request, month):
+    months = list(month_challenges.keys())
+    redirect_month = months[month-1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    return HttpResponseRedirect(redirect_path)
 
 
-def march(request):
-    return HttpResponse("Create a perpetual motion machine!")
+def monthly_challenge(request, month):
+
+    if month not in month_challenges:
+        return HttpResponseNotFound("<h1>No challenges for the month :(</h1>")
+
+    respose_data = f"<h1>{month_challenges[month]}</h1>"
+    return HttpResponse(respose_data)
